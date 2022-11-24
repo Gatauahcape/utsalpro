5f9f3888b9aaf45fd31cb46b63b8193c00af5846
Gatauahcape <115127434+Gatauahcape@users.noreply.github.com>
2022-11-24 10:31:02 +0700

    Create Variabel.java
    Tugas UTS No 5

---
+++ Variabel.java
@@ -0,0 +1,10 @@
+public class Variabel{
+public static void main(String[] args) {
+    
+String nama = "Muhammad Angga Nugraha";
+int Tahun_lahir = 2004;
+System.out.println("Assalamu'alaikum");
+System.out.println("Nama Saya : "+nama);
+System.out.println("Saya Lahir di Tahun : "+Tahun_lahir);
+  }
+}
